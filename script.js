15f6b66b7b75b6a58a3679f485ee06c886fa7abe
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-10-23 03:39:40 -0300

    Update index.html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let msgCokies = document.getElementById('icookies-msg');
+let msgCokies = document.getElementById('cookies-txt');
 
 function aceitar(){
     localStorage.lgpd = "sim"
     msgCokies.classList.remove('mostrar')
 }
-if (localStorage.lgpd == 'sim'){
-    msgCokies.classList.remove('mostrar')
+if (localStorage.lgpd == "sim"){
+    msgCokies.classList.remove('mostrar');
 } else {
-    msgCokies.classList.add('mostrar')
+    msgCokies.classList.add('mostrar');
 }
