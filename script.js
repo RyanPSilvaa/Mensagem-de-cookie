1e77edf6185bc7eff0f805558b4be9ae6e1ce4f5
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-10-23 22:00:19 -0300

    Atualizei index.html

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 
-
-let msgCookies = document.getElementById('cookies-txt');
-
-// Verifica se o usuário já aceitou os cookies
-function verificarCookies() {
-    if (localStorage.lgpd == 'sim') {
-        msgCookies.classList.remove("mostrar");
-    } else {
-        msgCookies.classList.add("mostrar");
-    }
+function func_Aceitar() {
+    document.getElementById("cookies-msg").style.display = "none";
 }
-
-// Função chamada ao aceitar os cookies
-function func_Aceitar(){
-    localStorage.lgpd = "sim";
-    msgCookies.classList.remove("mostrar");
-}
-
-// Chama a função para verificar o estado dos cookies ao carregar a página
-
